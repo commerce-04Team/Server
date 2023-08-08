9190129891eaaeb64f365b70277bdf10b742d3eb
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-08-08 22:40:20 +0900

    Feat : Wishlist Repository 생성

--- commerce/src/main/java/com/commerce_04/commerce/Repository/wishlist/repository/WishlistRepository.java
+++ commerce/src/main/java/com/commerce_04/commerce/Repository/wishlist/repository/WishlistRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
+	boolean existsByUserIdAndProductId(String userId, Long productId);
 }
