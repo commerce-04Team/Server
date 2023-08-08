46e13fec80103644379c5954930dc46d05bdc68f
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-08-08 13:07:02 +0900

    Feat : Inquiry 수정 User Entity와 연관관계 추가

--- commerce/src/main/java/com/commerce_04/commerce/Repository/inquiry/entity/Inquiry.java
+++ commerce/src/main/java/com/commerce_04/commerce/Repository/inquiry/entity/Inquiry.java
@@ -1,11 +1,15 @@
 package com.commerce_04.commerce.Repository.inquiry.entity;
 
+import com.commerce_04.commerce.Repository.user.Entity.User;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,6 +28,10 @@ public class Inquiry {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "inquiry_id", nullable = false)
 	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
 
 	@Column(name = "text", nullable = false)
 	private String text;
