9ab93906c1efeb957cf3b3d2ef0e150a204f2261
hjm12223 <gwa5068@gmail.com>
2023-08-08 22:51:46 +0900

    feat : UserPrincipal 의 대한 Entity 및 Repository 이며, UserPrincipalRoles 의 대한 Entity 및 Repository 입니다

---
+++ commerce/src/main/java/com/commerce_04/commerce/Repository/userPrincipal/UserPrincipalRolesRepository.java
@@ -0,0 +1,9 @@
+package com.commerce_04.commerce.Repository.userPrincipal;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserPrincipalRolesRepository extends JpaRepository<UserPrincipalRoles, Integer> {
+}
