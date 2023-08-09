f645bdec6af4e78980a92662a066720aa432d0cb
chaehyeonkim <rlacoguskch603@naver.com>
2023-08-09 12:09:37 +0900

    Feat : ProductRepository 상품목록,상세 추가

--- commerce/src/main/java/com/commerce_04/commerce/Repository/product/repository/ProductRepository.java
+++ commerce/src/main/java/com/commerce_04/commerce/Repository/product/repository/ProductRepository.java
@@ -3,6 +3,12 @@ package com.commerce_04.commerce.Repository.product.repository;
 import com.commerce_04.commerce.Repository.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllProducts();
+    Optional<Product> findProductByID(Long id);
 
 }
