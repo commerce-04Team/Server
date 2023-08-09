1eb56a9ba0b427cf74e11544ddcff617dd53b9e8
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-08-09 13:32:12 +0900

    Feat : WishlistService 구현 deleteWishlist 메소드 구현

--- commerce/src/main/java/com/commerce_04/commerce/Repository/wishlist/repository/WishlistRepository.java
+++ commerce/src/main/java/com/commerce_04/commerce/Repository/wishlist/repository/WishlistRepository.java
@@ -1,6 +1,7 @@
 package com.commerce_04.commerce.Repository.wishlist.repository;
 
 import com.commerce_04.commerce.Repository.wishlist.entity.Wishlist;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
 	boolean existsByUserIdAndProductId(String userId, Long productId);
+
+	Optional<Wishlist> findByUserIdAndProductId(String userId, Long productId);
 }
--- commerce/src/main/java/com/commerce_04/commerce/Service/wishlist/WishlistService.java
+++ commerce/src/main/java/com/commerce_04/commerce/Service/wishlist/WishlistService.java
@@ -6,10 +6,12 @@ import com.commerce_04.commerce.Repository.product.entity.Product;
 import com.commerce_04.commerce.Repository.product.repository.ProductRepository;
 import com.commerce_04.commerce.Repository.user.Entity.User;
 import com.commerce_04.commerce.Repository.user.Entity.UserRepository;
+import com.commerce_04.commerce.Repository.wishlist.entity.Wishlist;
 import com.commerce_04.commerce.Repository.wishlist.repository.WishlistRepository;
 import com.commerce_04.commerce.Service.wishlist.exception.WishlistErrorCode;
 import com.commerce_04.commerce.Service.wishlist.exception.WishlistException;
 import com.commerce_04.commerce.web.dto.wishlist.AddWishlistRequest;
+import com.commerce_04.commerce.web.dto.wishlist.DeleteWishlistRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -28,31 +30,55 @@ public class WishlistService {
 		String inputUserId = addWishlistRequest.getUserId();
 		Long inputProductId = addWishlistRequest.getProductId();
 
-		// 존재하는 유저인지 확인해야 함
-		// 예외처리 Refactoring 필요
-		User user = userRepository.findById(inputUserId)
-			.orElseThrow(() -> new RuntimeException("존재하지 않는 유저 입니다."));
+		User validatedUser = verifyMember(inputUserId);
+		Product validatedProduct = verifyProduct(inputProductId);
 
-		// 존재하는 상품인지 확인해야 함
-		// 예외처리 Refactoring 필요
-		Product product = productRepository.findById(inputProductId)
-			.orElseThrow(() -> new RuntimeException("존재하지 않는 상품 입니다."));
+		isWishlistExist(validatedUser, validatedProduct);
 
-		// 현재 판매 중인지 확인해야 함
+		wishlistRepository.save(toEntity(validatedUser, validatedProduct));
+	}
 
+	public void deleteWishlist(DeleteWishlistRequest deleteWishlistRequest) {
 
-		// 관심 목록에 등록돼 있는지 확인해야 함
-		if (isProductExist(user, product)) {
-			throw new WishlistException(WishlistErrorCode.PRODUCTS_ON_WISHLIST);
+		String inputUserId = deleteWishlistRequest.getUserId();
+		Long inputProductId = deleteWishlistRequest.getProductId();
+		Long inputWishlistId = deleteWishlistRequest.getWishlistId();
+
+		User validatedUser = verifyMember(inputUserId);
+		Product validatedProduct = verifyProduct(inputProductId);
+		Wishlist validatedWishlist = ValidatedWishlist(inputWishlistId);
+
+		if (validatedWishlist.equals(
+			isWishlistExist(validatedUser, validatedProduct))) {
+			wishlistRepository.delete(validatedWishlist);
+		} else {
+			throw new WishlistException(
+				WishlistErrorCode.PRODUCTS_ON_THE_WISHLIST);
 		}
-
-		// 관심 목록에 추가
-		wishlistRepository.save(toEntity(user, product));
 	}
 
-	private boolean isProductExist(User user, Product product) {
-		return wishlistRepository.existsByUserIdAndProductId(
-			user.getId(),
-			product.getId());
+	private Wishlist ValidatedWishlist(Long inputWishlistId) {
+		return wishlistRepository.findById(
+				inputWishlistId)
+			.orElseThrow(() -> new RuntimeException("존재하지 않는 상품 입니다."));
 	}
+
+	private Product verifyProduct(Long inputProductId) {
+		return productRepository.findById(inputProductId)
+			.orElseThrow(() -> new RuntimeException("존재하지 않는 상품 입니다."));
+	}
+
+	private User verifyMember(String inputUserId) {
+		return userRepository.findById(inputUserId)
+			.orElseThrow(() -> new RuntimeException("존재하지 않는 유저 입니다."));
+	}
+
+	private Wishlist isWishlistExist(User user, Product product) {
+		return wishlistRepository.findByUserIdAndProductId(
+				user.getId(), product.getId())
+			.orElseThrow(() -> new WishlistException(
+				WishlistErrorCode.PRODUCTS_IS_NOT_ON_THE_WISHLIST));
+	}
+
+
 }
