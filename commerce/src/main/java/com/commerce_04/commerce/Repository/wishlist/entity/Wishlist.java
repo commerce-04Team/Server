cf435ccd76472489cec6fca73a1ce6dc65020d83
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-08-09 12:29:13 +0900

    Feat : Wishlist Entity 코드 추가 @EqualsAndHashCode 추가 : Entity 일치 여부 확인을 위해

--- commerce/src/main/java/com/commerce_04/commerce/Repository/wishlist/entity/Wishlist.java
+++ commerce/src/main/java/com/commerce_04/commerce/Repository/wishlist/entity/Wishlist.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +22,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @Builder
 public class Wishlist {
 
